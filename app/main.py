0b4c8a181e6472a2a5eb77b11d72f653666b43cd
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-08 20:07:20 -0300

    feat: Environment variable

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 
+from app.configs.Environment import get_environment_variables
 from app.routers import user
 
-app = FastAPI()
+# Application Environment Configuration
+env = get_environment_variables()
 
+# Core Application Instance
+app = FastAPI(title=env.APP_NAME, version=env.API_VERSION)
+
+# Add Routers
 app.include_router(user.router, prefix="/v1", tags=["users"])
 
 
+# Health
 @app.get("/v1/health")
 async def root():
-    return {"message": "The API is LIVE!!"}
+    return {"version": env.API_VERSION}
