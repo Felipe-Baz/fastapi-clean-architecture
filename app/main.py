1d1e3532bb5f6b1203e07b71c319c12b3178933e
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-08 18:25:45 -0300

    feat: Health route

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ from app.routers import user
 app = FastAPI()
 
 app.include_router(user.router, prefix="/v1", tags=["users"])
+
+
+@app.get("/v1/health")
+async def root():
+    return {"message": "The API is LIVE!!"}
